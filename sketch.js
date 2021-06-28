483fd086d114b5571840b5806d53c1bb302f2c90
Divya-coder2006 <83022130+Divya-coder2006@users.noreply.github.com>
2021-06-28 19:09:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,9 +7,10 @@ const Body = Matter.Body;
 var ball;
 var log1,log2,log3  ;
 var ground,ground2,ground3,ground4;
-var dustBinImg;
+var crImg;
 
 function preload(){
+	crImg=loadImage("cr.png")
 }
 
 function setup() {
@@ -23,7 +24,7 @@ function setup() {
 	ground2 = new Ground(10,375,20,730);
 	ground3= new Ground(1590,375,20,730);
 	ground4 = new Ground(800,10,1600,20);
-	bin = new Dustbin(1200,740);
+	bin = new Dustbin(1180,740);
 	ball=new Ball(80,420,45,45);
 
 
@@ -32,9 +33,10 @@ function setup() {
 }
 
 
-function draw() {
+function draw() {  
+	 background(crImg);
   rectMode(CENTER);
-  background(0);
+
   
   
   ground.display();
@@ -42,17 +44,17 @@ function draw() {
   ground3.display();
   ground4.display();
   bin.display();
-
   ball.display();
 
 
   drawSprites();
- 
+
 }
 
 function keyPressed() {
 	if (keyCode=== UP_ARROW) {
-	  Matter.Body.applyForce(ball.body,ball.body.position,{x:80,y:-80});
+	  Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-80});
 	}
 }
+
 
